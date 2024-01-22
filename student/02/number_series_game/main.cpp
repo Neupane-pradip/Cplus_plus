fbdf113e4244a46f8140917da5d467d1965bad44
Pradip Neupane <pradip.neupane@tuni.fi>
2024-01-22 17:57:35 +0200

    Reading the input and printing the numbers

---
+++ student/02/number_series_game/main.cpp
@@ -0,0 +1,29 @@
+
+/*
+* Programming 2:C++ Spring 2024.
+
+* @author Pradip Neupane (pradipneupane@tuni.fi).
+* StudentId: 150426327
+*/
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    std::cout << "How many numbers would you like to have? ";
+
+    // Write your code here
+    int n;
+    cin>>n;
+
+    for(int i=1; i<=n;++i)
+    {
+        cout<<i<<endl;
+    }
+
+
+    return 0;
+}
