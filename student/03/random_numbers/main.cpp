1fa5cbc0ae540ea4096ba8c455041d5c06b75368
Pradip Neupane <pradip.neupane@tuni.fi>
2024-01-28 21:15:43 +0200

    Random number is generated

---
+++ student/03/random_numbers/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <random>
+#include <string>
+
+using namespace std;
+
+void produce_random_numbers(unsigned int lower, unsigned int upper)
+{
+
+    // Implement your function here
+     unsigned int seed=0;
+     char key;
+
+    cout<<"Enter a seed value: ";
+    cin>>seed;
+    default_random_engine gen(seed);
+     uniform_int_distribution <int> m(lower, upper);
+     do{
+
+
+    cout<<"Your drawn random number is " <<m(gen)<<endl;
+    cout<<"Press q to quit or any other key to continue: ";
+    cin>>key;
+       }
+     while(key!='q');
+}
+
+int main()
+{
+    unsigned int lower_bound, upper_bound;
+    cout << "Enter a lower bound: ";
+    cin >> lower_bound;
+    cout << "Enter an upper bound: ";
+    cin >> upper_bound;
+
+    if(lower_bound >= upper_bound)
+    {
+        cout << "The upper bound must be strictly greater than the lower bound"
+             << endl;
+        return EXIT_FAILURE;
+    }
+
+    produce_random_numbers(lower_bound, upper_bound);
+
+    return EXIT_SUCCESS;
+}
