21899b00bdd7b44f140f580faebc5300d50a6aaa
Pradip Neupane <pradip.neupane@tuni.fi>
2024-03-20 22:07:06 +0200

    Implemeting remaining function

--- student/09/arrays/array_operations.cpp
+++ student/09/arrays/array_operations.cpp
@@ -19,15 +19,42 @@ int greatest_v1(int *itemptr, int size)
 
 int greatest_v2(int *itemptr, int *endptr)
 {
+    int greatest = *itemptr;
 
+        while (itemptr != endptr)
+        {
+            int current = *(itemptr++);
+            if (current > greatest)
+            {
+                greatest = current;
+            }
+        }
+        return greatest;
 }
 
 void copy(int *itemptr, int *endptr, int *targetptr)
 {
-
+    while (itemptr != endptr)
+        {
+            *targetptr = *itemptr;
+            itemptr++;
+            targetptr++;
+        }
 }
 
 void reverse(int *leftptr, int *rightptr)
 {
+    rightptr--;
 
+       int temp;
+
+       while (leftptr < rightptr)
+       {
+           temp = *leftptr;
+           *leftptr = *rightptr;
+           *rightptr = temp;
+
+           leftptr++;
+           rightptr--;
+       }
 }
