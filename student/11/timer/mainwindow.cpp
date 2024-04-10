0fa07ccbc25f4cca08525e643b691615e40d3d16
Pradip Neupane <pradip.neupane@tuni.fi>
2024-04-10 22:00:41 +0300

    program is okay

---
+++ student/11/timer/mainwindow.cpp
@@ -0,0 +1,59 @@
+#include "mainwindow.hh"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->lcdNumber->setPalette(Qt::red);
+    ui->lcdNumber_2->setPalette(Qt::green);
+    timer= new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(runTheClock()));
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_startButton_clicked()
+{
+    if(!startPressed){
+        timer->start(1000);
+    }
+    startPressed=true;
+}
+
+void MainWindow::runTheClock()
+{
+    if(secs<60){
+        ++secs;
+    }
+    else{
+        secs=0;
+        ++mins;
+    }
+    QString sec= QString::number(secs);
+    QString min= QString::number(mins);
+    ui->lcdNumber_2->display(sec);
+    ui->lcdNumber->display(min);
+}
+
+void MainWindow::on_stopButton_clicked()
+{
+    timer->stop();
+    startPressed=false;
+}
+
+void MainWindow::on_resetButton_clicked()
+{
+    on_stopButton_clicked();
+    secs=0;
+    mins=0;
+    QString sec= QString::number(secs);
+    QString min= QString::number(mins);
+    ui->lcdNumber_2->display(sec);
+    ui->lcdNumber->display(min);
+}
